931a784c3bf161821b140cf88c043200f87a134e
JIAN PAN <1057237562@outlook.com>
2023-12-22 16:34:42 +0000

    pending for tensor update

--- layer.cpp
+++ layer.cpp
@@ -6,16 +6,18 @@
 
 using namespace mutil;
 
-class Layer {
+class Layer
+{
 
 public:
-    virtual Mat forward(Mat& in) = 0;
-    virtual Mat backward(Mat& in) = 0;
-    virtual void randomize(default_random_engine& e) = 0;
-    virtual void learn(Optimizer* optimizer) = 0;
+    virtual Mat forward(Mat &in) = 0;
+    virtual Mat backward(Mat &in) = 0;
+    virtual void randomize(default_random_engine &e) = 0;
+    virtual void learn(Optimizer *optimizer) = 0;
 };
 
-class FlattenLayer : public Layer {
+class FlattenLayer : public Layer
+{
 public:
     int h, w;
     FlattenLayer(int h, int w)
@@ -23,56 +25,61 @@ public:
         this->h = h;
         this->w = w;
     }
-    Mat forward(Mat& in)
+    Mat forward(Mat &in)
     {
         Mat ret(1, h * w);
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
                 ret[0][i * w + j] = in[i][j];
             }
         }
         return ret;
     }
-    Mat backward(Mat& in)
+    Mat backward(Mat &in)
     {
         Mat ret(h, w);
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
+        for (int i = 0; i < h; i++)
+        {
+            for (int j = 0; j < w; j++)
+            {
                 ret[i][j] = in[0][i * w + j];
             }
         }
         return ret;
     }
 
-    void randomize(default_random_engine& e)
+    void randomize(default_random_engine &e)
     {
     }
 
-    void learn(Optimizer* optimizer)
+    void learn(Optimizer *optimizer)
     {
     }
 };
 
-class LinearLayer : public Layer {
+class LinearLayer : public Layer
+{
 protected:
     Mat x, y;
 
 public:
     int in, out;
     LinearLayer(int in, int out)
-        : x(1, in)
-        , y(1, out)
+        : x(1, in), y(1, out)
     {
         this->in = in;
         this->out = out;
     }
-    virtual Mat forward(Mat& in) = 0;
-    virtual Mat backward(Mat& in) = 0;
-    virtual void randomize(default_random_engine& e) = 0;
-    virtual void learn(Optimizer* optimizer) = 0;
+    virtual Mat forward(Mat &in) = 0;
+    virtual Mat backward(Mat &in) = 0;
+    virtual void randomize(default_random_engine &e) = 0;
+    virtual void learn(Optimizer *optimizer) = 0;
 };
 
-class DenseLayer : public LinearLayer {
+class DenseLayer : public LinearLayer
+{
 protected:
     Mat w, b;
 
@@ -80,24 +87,18 @@ public:
     Mat delta_w, delta_b;
     Mat nabla_w, nabla_b;
     DenseLayer(int in, int out)
-        : LinearLayer(in, out)
-        , w(in, out)
-        , b(1, out)
-        , delta_w(in, out)
-        , delta_b(1, out)
-        , nabla_w(in, out)
-        , nabla_b(1, out)
+        : LinearLayer(in, out), w(in, out), b(1, out), delta_w(in, out), delta_b(1, out), nabla_w(in, out), nabla_b(1, out)
     {
         nabla_w.clear(), nabla_b.clear();
     }
-    Mat forward(Mat& in)
+    Mat forward(Mat &in)
     {
         Mat ret = in * w + b;
         x = in;
         y = ret;
         return ret;
     }
-    Mat backward(Mat& in)
+    Mat backward(Mat &in)
     {
         delta_w = x.transpose() * in;
         delta_b = in;
@@ -107,12 +108,12 @@ public:
         return in * wT;
     }
 
-    void randomize(default_random_engine& e)
+    void randomize(default_random_engine &e)
     {
         w.randomize(e), b.randomize(e);
     }
 
-    void learn(Optimizer* optimizer)
+    void learn(Optimizer *optimizer)
     {
         w = optimizer->optimize(w, nabla_w);
         b = optimizer->optimize(b, nabla_b);
@@ -121,7 +122,8 @@ public:
     }
 };
 
-class SigmoidLayer : public Layer {
+class SigmoidLayer : public Layer
+{
 protected:
     Mat x, y;
 
@@ -129,27 +131,28 @@ public:
     SigmoidLayer()
     {
     }
-    Mat forward(Mat& in)
+    Mat forward(Mat &in)
     {
         x = in;
         y = mutil::sigmoid(in);
         return in;
     }
-    Mat backward(Mat& in)
+    Mat backward(Mat &in)
     {
         return in.dot(mutil::sigmoid_prime(x));
     }
 
-    void randomize(default_random_engine& e)
+    void randomize(default_random_engine &e)
     {
     }
 
-    void learn(Optimizer* optimizer)
+    void learn(Optimizer *optimizer)
     {
     }
 };
 
-class RELULayer : public Layer {
+class RELULayer : public Layer
+{
 protected:
     Mat x, y;
 
@@ -157,71 +160,73 @@ public:
     RELULayer()
     {
     }
-    Mat forward(Mat& in)
+    Mat forward(Mat &in)
     {
         x = in;
         y = mutil::relu(in);
         return in;
     }
-    Mat backward(Mat& in)
+    Mat backward(Mat &in)
     {
         return in.dot(mutil::relu_prime(x));
     }
 
-    void randomize(default_random_engine& e)
+    void randomize(default_random_engine &e)
     {
     }
 
-    void learn(Optimizer* optimizer)
+    void learn(Optimizer *optimizer)
     {
     }
 };
 
-class ConvLayer : public Layer {
+class ConvLayer : public Layer
+{
 protected:
     Mat x, y;
-    int kernel_size, stride, padding;
+    array<int, 3> in_size, kernel_size;
+
+    int stride, padding;
     Mat w, b;
     Mat delta_w, delta_b;
     Mat nabla_w, nabla_b;
 
 public:
-    ConvLayer(int kernel_count, int kernel_size, int stride, int padding)
-        : w(kernel_size * kernel_size, kernel_count)
-        , b(1, kernel_count)
-        , delta_w(kernel_size * kernel_size, kernel_count)
-        , delta_b(1, kernel_count)
-        , nabla_w(kernel_size * kernel_size, kernel_count)
-        , nabla_b(1, kernel_count)
+    ConvLayer(int width, int height, int channel, int kernel_height, int kernel_width, int kernel_count, int stride, int padding)
+        : w(kernel_count, kernel_height * kernel_width), b(1, kernel_count), delta_w(kernel_count, kernel_height * kernel_width), delta_b(1, kernel_count), nabla_w(kernel_count, kernel_height * kernel_width), nabla_b(1, kernel_count)
     {
-        this->kernel_size = kernel_size;
+        this->in_size = {width, height, channel};
+        this->kernel_size = {kernel_height, kernel_width, kernel_count};
         this->stride = stride;
         this->padding = padding;
         nabla_w.clear(), nabla_b.clear();
     }
-    Mat forward(Mat& in)
+    Mat forward(Mat &in)
     {
-        Mat ret = mutil::conv(in, w, stride, padding) + b;
+        Mat ret = mutil::conv(in, w, stride, padding) + b; // this is not correct
+        for (int i = 0; i < kernel_count; i++)
+        {
+        }
         x = in;
         y = ret;
         return ret;
     }
-    Mat backward(Mat& in)
+    Mat backward(Mat &in)
     {
         delta_w = mutil::conv(x, in, stride, padding);
         delta_b = in;
         nabla_w += delta_w;
         nabla_b += delta_b;
         Mat wT = w.transpose();
-        return mutil::deconv(in, wT, stride, padding);
+        return mutil::deconv(in, wT, stride, padding); // this is not correct
     }
 
-    void randomize(default_random_engine& e)
+    void randomize(default_random_engine &e)
     {
         w.randomize(e), b.randomize(e);
     }
 
-    void learn(Optimizer* optimizer)
+    void learn(Optimizer *optimizer)
     {
         w = optimizer->optimize(w, nabla_w);
         b = optimizer->optimize(b, nabla_b);
--- mutil.cpp
+++ mutil.cpp
@@ -10,266 +10,323 @@
 
 using namespace std;
 
-namespace mutil {
-
-static int copyCount = 0;
-static int constructTime = 0;
-static int multiplyTime = 0;
-static int multiplyCount = 0;
-
-class Vec {
-
-public:
-    vector<float> val;
-    int length;
-    Vec(int len)
-    {
-        length = len;
-        val = vector<float>(length);
-    }
-
-    float& operator[](int index)
-    {
-        assert(index >= 0 && index < length);
-        return val[index];
-    }
-
-    void randomize(default_random_engine& e)
-    {
-        normal_distribution<float> u(0, 1);
-        for (int i = 0; i < length; i++) {
-            val[i] = u(e);
-        }
-    }
-};
-
-class Mat {
-    vector<float> val;
-
-public:
-    pair<int, int> size;
-    Mat() { }
-    Mat(int m, int n)
-        : val(m * n)
-    {
-        auto start = clock();
-        auto end = clock();
-        constructTime += end - start;
-        size = { m, n };
-    }
-
-    auto operator[](int index)
-    {
-        assert(index >= 0 && index < size.first);
-        return val.begin() + index * size.second;
-    }
-
-    Mat& dot(Mat& other)
-    {
-        assert(size.first == other.size.first && size.second == other.size.second);
-        for (int i = 0; i < size.first; i++) {
-            for (int j = 0; j < size.second; j++) {
-                (*this)[i][j] *= other[i][j];
-            }
-        }
-        return *this;
-    }
-
-    Mat& operator+(Mat& other)
-    {
-        assert(size.first == other.size.first && size.second == other.size.second);
-        for (int i = 0; i < size.first; i++) {
-            for (int j = 0; j < size.second; j++) {
-                (*this)[i][j] += other[i][j];
-            }
-        }
-        return *this;
-    }
-
-    Mat& operator+=(Mat& other)
-    {
-        assert(size.first == other.size.first && size.second == other.size.second);
-        for (int i = 0; i < size.first; i++) {
-            for (int j = 0; j < size.second; j++) {
-                (*this)[i][j] += other[i][j];
-            }
-        }
-        return *this;
-    }
-
-    Mat& operator-(Mat& other)
-    {
-        assert(size.first == other.size.first && size.second == other.size.second);
-        for (int i = 0; i < size.first; i++) {
-            for (int j = 0; j < size.second; j++) {
-                (*this)[i][j] -= other[i][j];
-            }
-        }
-        return *this;
-    }
-
-    Mat& operator-=(Mat& other)
-    {
-        assert(size.first == other.size.first && size.second == other.size.second);
-        for (int i = 0; i < size.first; i++) {
-            for (int j = 0; j < size.second; j++) {
-                (*this)[i][j] -= other[i][j];
-            }
-        }
-        return *this;
-    }
-
-    Mat operator*(Mat& other)
-    {
-        assert(size.second == other.size.first);
-        auto start = clock();
-        Mat res(size.first, other.size.second);
-        float r;
-        for (int i = 0; i < size.first; ++i)
-            for (int k = 0; k < size.second; ++k) {
-                r = (*this)[i][k];
-                for (int j = 0; j < other.size.second; ++j)
-                    res[i][j] += other[k][j] * r;
-            }
-        auto end = clock();
-        multiplyTime += end - start;
-        ++multiplyCount;
-        return res;
-    }
-
-    Mat& operator*(float& theta)
-    {
-        for (int i = 0; i < size.first; i++) {
-            for (int j = 0; j < size.second; j++) {
-                (*this)[i][j] *= theta;
-            }
-        }
-        return *this;
-    }
-
-    Mat transpose()
-    {
-        Mat res(size.second, size.first);
-        for (int i = 0; i < size.first; i++) {
-            for (int j = 0; j < size.second; j++) {
-                res[j][i] = (*this)[i][j];
-            }
-        }
-        return res;
-    }
-
-    void randomize(default_random_engine& e)
-    {
-        normal_distribution<float> u(0, 1);
-        for (int i = 0; i < size.first; i++) {
-            for (int j = 0; j < size.second; j++) {
-                (*this)[i][j] = u(e);
-            }
-        }
-    }
-
-    void clear()
-    {
-        for (int i = 0; i < size.first; i++) {
-            for (int j = 0; j < size.second; j++) {
-                (*this)[i][j] = 0;
-            }
-        }
-    }
-};
-
-Mat& sigmoid(Mat& in)
+namespace mutil
 {
-    for (int i = 0; i < in.size.first; i++) {
-        for (int j = 0; j < in.size.second; j++) {
-            in[i][j] = 1 / (1 + exp(-in[i][j]));
-        }
-    }
-    return in;
-}
 
-Mat& sigmoid_prime(Mat& in)
-{
-    for (int i = 0; i < in.size.first; i++) {
-        for (int j = 0; j < in.size.second; j++) {
-            float s = 1 / (1 + exp(-in[i][j]));
-            in[i][j] = s * (1 - s);
-        }
-    }
-    return in;
-}
+    static int copyCount = 0;
+    static int constructTime = 0;
+    static int multiplyTime = 0;
+    static int multiplyCount = 0;
 
-Mat& relu(Mat& in)
-{
-    for (int i = 0; i < in.size.first; i++) {
-        for (int j = 0; j < in.size.second; j++) {
-            in[i][j] = max(0.0f, in[i][j]);
-        }
-    }
-    return in;
-}
+    class Vec
+    {
 
-Mat& relu_prime(Mat& in)
-{
-    for (int i = 0; i < in.size.first; i++) {
-        for (int j = 0; j < in.size.second; j++) {
-            in[i][j] = in[i][j] > 0 ? 1 : 0;
+    public:
+        vector<float> val;
+        int length;
+        Vec(int len)
+        {
+            length = len;
+            val = vector<float>(length);
         }
-    }
-    return in;
-}
 
-Mat conv(Mat& in, Mat& kernel, int stride, int padding)
-{
-    int in_height = in.size.first;
-    int in_width = in.size.second;
-    int kernel_height = kernel.size.first;
-    int kernel_width = kernel.size.second;
-    int out_height = (in_height - kernel_height + 2 * padding) / stride + 1;
-    int out_width = (in_width - kernel_width + 2 * padding) / stride + 1;
-    Mat out(out_height, out_width);
-    for (int i = 0; i < out_height; i++) {
-        for (int j = 0; j < out_width; j++) {
-            float sum = 0;
-            for (int k = 0; k < kernel_height; k++) {
-                for (int l = 0; l < kernel_width; l++) {
-                    int x = i * stride + k - padding;
-                    int y = j * stride + l - padding;
-                    if (x >= 0 && x < in_height && y >= 0 && y < in_width) {
-                        sum += in[x][y] * kernel[k][l];
-                    }
+        float &operator[](int index)
+        {
+            assert(index >= 0 && index < length);
+            return val[index];
+        }
+
+        void randomize(default_random_engine &e)
+        {
+            normal_distribution<float> u(0, 1);
+            for (int i = 0; i < length; i++)
+            {
+                val[i] = u(e);
+            }
+        }
+    };
+
+    class Mat
+    {
+        vector<float> val;
+
+    public:
+        pair<int, int> size;
+        Mat() {}
+        Mat(int m, int n)
+            : val(m * n)
+        {
+            size = {m, n};
+        }
+
+        Mat(int m,int n,vector<float> &v)
+            : val(v)
+        {
+            size = {m, n};
+        }
+
+        auto operator[](int index)
+        {
+            assert(index >= 0 && index < size.first);
+            return val.begin() + index * size.second;
+        }
+
+        Mat &dot(Mat &other)
+        {
+            assert(size.first == other.size.first && size.second == other.size.second);
+            for (int i = 0; i < size.first; i++)
+            {
+                for (int j = 0; j < size.second; j++)
+                {
+                    (*this)[i][j] *= other[i][j];
                 }
             }
-            out[i][j] = sum;
+            return *this;
         }
-    }
-    return out;
-}
 
-Mat deconv(Mat& in, Mat& kernel, int stride, int padding)
-{
-    int in_height = in.size.first;
-    int in_width = in.size.second;
-    int kernel_height = kernel.size.first;
-    int kernel_width = kernel.size.second;
-    int out_height = (in_height - 1) * stride + kernel_height - 2 * padding;
-    int out_width = (in_width - 1) * stride + kernel_width - 2 * padding;
-    Mat out(out_height, out_width);
-    for (int i = 0; i < in_height; i++) {
-        for (int j = 0; j < in_width; j++) {
-            for (int k = 0; k < kernel_height; k++) {
-                for (int l = 0; l < kernel_width; l++) {
-                    int x = i * stride + k - padding;
-                    int y = j * stride + l - padding;
-                    if (x >= 0 && x < out_height && y >= 0 && y < out_width) {
-                        out[x][y] += in[i][j] * kernel[k][l];
-                    }
+        Mat &operator+(Mat &other)
+        {
+            assert(size.first == other.size.first && size.second == other.size.second);
+            for (int i = 0; i < size.first; i++)
+            {
+                for (int j = 0; j < size.second; j++)
+                {
+                    (*this)[i][j] += other[i][j];
+                }
+            }
+            return *this;
+        }
+
+        Mat &operator+=(Mat &other)
+        {
+            assert(size.first == other.size.first && size.second == other.size.second);
+            for (int i = 0; i < size.first; i++)
+            {
+                for (int j = 0; j < size.second; j++)
+                {
+                    (*this)[i][j] += other[i][j];
+                }
+            }
+            return *this;
+        }
+
+        Mat &operator-(Mat &other)
+        {
+            assert(size.first == other.size.first && size.second == other.size.second);
+            for (int i = 0; i < size.first; i++)
+            {
+                for (int j = 0; j < size.second; j++)
+                {
+                    (*this)[i][j] -= other[i][j];
+                }
+            }
+            return *this;
+        }
+
+        Mat &operator-=(Mat &other)
+        {
+            assert(size.first == other.size.first && size.second == other.size.second);
+            for (int i = 0; i < size.first; i++)
+            {
+                for (int j = 0; j < size.second; j++)
+                {
+                    (*this)[i][j] -= other[i][j];
+                }
+            }
+            return *this;
+        }
+
+        Mat operator*(Mat &other)
+        {
+            assert(size.second == other.size.first);
+            auto start = clock();
+            Mat res(size.first, other.size.second);
+            float r;
+            for (int i = 0; i < size.first; ++i)
+                for (int k = 0; k < size.second; ++k)
+                {
+                    r = (*this)[i][k];
+                    for (int j = 0; j < other.size.second; ++j)
+                        res[i][j] += other[k][j] * r;
+                }
+            auto end = clock();
+            multiplyTime += end - start;
+            ++multiplyCount;
+            return res;
+        }
+
+        Mat &operator*(float &theta)
+        {
+            for (int i = 0; i < size.first; i++)
+            {
+                for (int j = 0; j < size.second; j++)
+                {
+                    (*this)[i][j] *= theta;
+                }
+            }
+            return *this;
+        }
+
+        Mat transpose()
+        {
+            Mat res(size.second, size.first);
+            for (int i = 0; i < size.first; i++)
+            {
+                for (int j = 0; j < size.second; j++)
+                {
+                    res[j][i] = (*this)[i][j];
+                }
+            }
+            return res;
+        }
+
+        void randomize(default_random_engine &e)
+        {
+            normal_distribution<float> u(0, 1);
+            for (int i = 0; i < size.first; i++)
+            {
+                for (int j = 0; j < size.second; j++)
+                {
+                    (*this)[i][j] = u(e);
                 }
             }
         }
+
+        void clear()
+        {
+            for (int i = 0; i < size.first; i++)
+            {
+                for (int j = 0; j < size.second; j++)
+                {
+                    (*this)[i][j] = 0;
+                }
+            }
+        }
+    };
+
+    class Kernel
+    {
+        auto val;
+        pair<int, int> size;
+
+    public:
+        Kernel(auto list, int height, int width)
+        {
+            this->size = {height, width};
+            this->val = list;
+        }
+    };
+
+    Mat &sigmoid(Mat &in)
+    {
+        for (int i = 0; i < in.size.first; i++)
+        {
+            for (int j = 0; j < in.size.second; j++)
+            {
+                in[i][j] = 1 / (1 + exp(-in[i][j]));
+            }
+        }
+        return in;
     }
-    return out;
-}
+
+    Mat &sigmoid_prime(Mat &in)
+    {
+        for (int i = 0; i < in.size.first; i++)
+        {
+            for (int j = 0; j < in.size.second; j++)
+            {
+                float s = 1 / (1 + exp(-in[i][j]));
+                in[i][j] = s * (1 - s);
+            }
+        }
+        return in;
+    }
+
+    Mat &relu(Mat &in)
+    {
+        for (int i = 0; i < in.size.first; i++)
+        {
+            for (int j = 0; j < in.size.second; j++)
+            {
+                in[i][j] = max(0.0f, in[i][j]);
+            }
+        }
+        return in;
+    }
+
+    Mat &relu_prime(Mat &in)
+    {
+        for (int i = 0; i < in.size.first; i++)
+        {
+            for (int j = 0; j < in.size.second; j++)
+            {
+                in[i][j] = in[i][j] > 0 ? 1 : 0;
+            }
+        }
+        return in;
+    }
+
+    Mat conv(Kernel &in, Kernel &kernel, int stride, int padding)
+    {
+        int in_height = in.size.first;
+        int in_width = in.size.second;
+        int kernel_height = kernel.size.first;
+        int kernel_width = kernel.size.second;
+        int out_height = (in_height - kernel_height + 2 * padding) / stride + 1;
+        int out_width = (in_width - kernel_width + 2 * padding) / stride + 1;
+        Mat out(out_height, out_width);
+        for (int i = 0; i < out_height; i++)
+        {
+            for (int j = 0; j < out_width; j++)
+            {
+                float sum = 0;
+                for (int k = 0; k < kernel_height; k++)
+                {
+                    for (int l = 0; l < kernel_width; l++)
+                    {
+                        int x = i * stride + k - padding;
+                        int y = j * stride + l - padding;
+                        if (x >= 0 && x < in_height && y >= 0 && y < in_width)
+                        {
+                            sum += in[x][y] * kernel[k][l];
+                        }
+                    }
+                }
+                out[i][j] = sum;
+            }
+        }
+        return out;
+    }
+
+    Mat deconv(Mat &in, Mat &kernel, int stride, int padding)
+    {
+        int in_height = in.size.first;
+        int in_width = in.size.second;
+        int kernel_height = kernel.size.first;
+        int kernel_width = kernel.size.second;
+        int out_height = (in_height - 1) * stride + kernel_height - 2 * padding;
+        int out_width = (in_width - 1) * stride + kernel_width - 2 * padding;
+        Mat out(out_height, out_width);
+        for (int i = 0; i < in_height; i++)
+        {
+            for (int j = 0; j < in_width; j++)
+            {
+                for (int k = 0; k < kernel_height; k++)
+                {
+                    for (int l = 0; l < kernel_width; l++)
+                    {
+                        int x = i * stride + k - padding;
+                        int y = j * stride + l - padding;
+                        if (x >= 0 && x < out_height && y >= 0 && y < out_width)
+                        {
+                            out[x][y] += in[i][j] * kernel[k][l];
+                        }
+                    }
+                }
+            }
+        }
+        return out;
+    }
 }
 
 #endif
